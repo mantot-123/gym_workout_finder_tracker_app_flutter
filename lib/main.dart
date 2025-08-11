123c7933b10c264e64861603551665a3e24956b0
emman123 <67055497+mantot-123@users.noreply.github.com>
2025-08-11 11:55:40 +0800

    feat: Added an API key error page + check if an API key is set

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import "package:gym_workout_finder_tracker_app_flutter/pages/saved_workouts.dart";
 import "pages/home.dart";
 import "pages/search_form.dart";
 import "pages/saved_workouts.dart";
+import "pages/api_key_empty_error.dart";
+import "components/ui_scaffold.dart";
 
 void main() {
   runApp(MainApp());
@@ -16,13 +17,23 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  int selectedPage = 0; 
+  int selectedPage = 0;
+  bool apiKeySet = false;
+
   List<Widget> pages = [
     HomePage(),
     SearchForm(),
     SavedWorkoutsPage()
   ];
 
+  @override
+  void initState() {
+    // check if an API key is set before running
+    final String API_KEY = const String.fromEnvironment("API_KEY");
+    if(API_KEY != "") {
+      apiKeySet = true;
+    }
+  }
 
   void changePage(int newPage) {
     setState(() {
@@ -34,20 +45,22 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: pages[selectedPage],
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.lightGreen[200],
-          selectedItemColor: Colors.lightGreen[900],
-          currentIndex: selectedPage,
-          onTap: changePage,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-            BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: "Saved")
-          ]
-        )
-      ),
+      home: 
+        apiKeySet 
+        ? Scaffold(
+          body: pages[selectedPage],
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.lightGreen[200],
+            selectedItemColor: Colors.lightGreen[900],
+            currentIndex: selectedPage,
+            onTap: changePage,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+              BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: "Saved")
+            ]
+          ))
+        : APIKeyEmptyErrorPage()
     );
   }
 }
---
+++ lib/pages/api_key_empty_error.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import "../components/ui_scaffold.dart";
+
+class APIKeyEmptyErrorPage extends StatelessWidget {
+  APIKeyEmptyErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return UIScaffold(
+      appBarTitle: "Error",
+      scaffoldBody: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Error", textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Icon(Icons.error, size: 60),
+            SizedBox(height: 10),
+            Text("An API key has not been set for this app. Please set one and then try again.", textAlign: TextAlign.center)
+          ],
+        )
+      ) 
+    );
+  }
+}
