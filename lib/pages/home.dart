2de06a97486b8d3288f172b4d0466eb8ab09177f
emman123 <67055497+mantot-123@users.noreply.github.com>
2025-08-13 13:43:06 +0800

    refactor: Added image asset to pubspec.yaml

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.file(File("assets/logo.png"), height: 228, width: 228),
+            Image.asset("assets/logo.png", height: 228, width: 228),
             Text("Welcome!", textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             Icon(Icons.fitness_center, size: 60),
             SizedBox(height: 10),
